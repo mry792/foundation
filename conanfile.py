edd830ed1cbbdd0d7d96ceaf28b82a22f5f2f165
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2022-08-14 21:14:04 -0500

    build(conan): add header-only conanfile

---
+++ conanfile.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python3
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import update_conandata
+from conan.tools.scm import Git
+from conans.tools import Git as LegacyGit
+
+
+class FoundationRecipe (ConanFile):
+    name = 'foundation'
+    # version = (computed from local repo)
+
+    author = 'M. Emery Goss <m.goss792@gmail.com>'
+    url = 'https://github.com/mry792/foundation.git'
+    description = (
+        'Collection of standard utilities. All support compiling for embedded '
+        'systems.'
+    )
+
+    settings = 'compiler', 'build_type'
+    generators = 'virtualenv', 'CMakeToolchain'
+
+    def package_id (self):
+        self.info.clear()
+
+    def set_version (self):
+        # git = LegacyGit(self.recipe_folder)
+        # tag = git.run('describe --tags')
+        # self.version = tag[1:]
+        self.version = '0.0.1'
+
+    def export (self):
+        git = Git(self, self.recipe_folder)
+        scm_url, scm_commit = git.get_url_and_commit()
+        update_conandata(self, {
+            'source': {
+                'commit': scm_commit,
+                'url': scm_url,
+            }
+        })
+
+    def source (self):
+        git = Git(self)
+        source = self.conan_data['source']
+        git.clone(source['url'], target = '.')
+        git.checkout(source['commit'])
+
+    def layout (self):
+        cmake_layout(self)
+
+    def build (self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package (self):
+        cmake = CMake(self)
+        cmake.install()
